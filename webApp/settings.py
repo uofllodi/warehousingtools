58cd057af8d0ccd3af32001b8b0dfdb0ab6b9620
Luis <lodiuofl@gmail.com>
2018-09-25 13:29:58 -0400

    change to lodi

--- webApp/settings.py
+++ webApp/settings.py
@@ -160,4 +160,4 @@ BROKER_HEARTBEAT = None # We're using TCP keep-alive instead
 BROKER_CONNECTION_TIMEOUT = 30 # May require a long timeout due to Linux DNS timeouts etc
 CELERY_EVENT_QUEUE_EXPIRES = 60 # Will delete all celeryev. queues without consumers after 1 minute.
 CELERYD_PREFETCH_MULTIPLIER = 1 # Disable prefetching, it's causes problems and doesn't help performance
-CELERYD_CONCURRENCY = 10
+CELERYD_CONCURRENCY = 50
