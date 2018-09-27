24b5e86edb5ba905c2b8e7c229cc070fd806a95b
Luis <lodiuofl@gmail.com>
2018-09-26 23:21:05 -0400

    try middleware for 503

--- webApp/settings.py
+++ webApp/settings.py
@@ -67,6 +67,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'webApp.503middleware.ExceptionMiddleware'
 ]
 
 ROOT_URLCONF = 'webApp.urls'
